6bec27c1481069b8004feec320ff2e9fb5d73df8
Cesar Guerrero <cesarguerrero@Cesars-MacBook-Pro.local>
2024-03-30 19:13:32 -0500

    feat: added mocks

---
+++ front_end/src/mocks/notes.ts
@@ -0,0 +1,23 @@
+export const mockShortNoteOne = {
+  id: 0,
+  title: "Note 1",
+  body: "This is a mock note",
+};
+
+export const mockShortNoteTwo = {
+  id: 1,
+  title: "Note 2",
+  body: "This is also a mock note",
+}
+
+export const mockLongNoteOne = {
+  id: 2,
+  title: "Long note 1",
+  body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus congue quam sit amet nisi pharetra vulputate. Nullam quis condimentum lorem, ut vehicula quam. Donec porttitor tortor sit amet est fermentum, a varius ipsum pellentesque. Quisque a ligula gravida, bibendum quam quis, mattis lacus.",
+};
+
+export const mockLongNoteTwo = {
+  id: 3,
+  title: "Long note 1",
+  body: "Vestibulum at metus augue. Aliquam et neque in justo dictum rutrum. Sed vitae condimentum nunc. Integer sed odio hendrerit tortor ultrices ultrices. Nullam eget eros eu lacus sagittis hendrerit. Quisque semper lacinia rutrum. Nam nisi est, ultrices ac placerat ac, efficitur sit amet massa.",
+};
